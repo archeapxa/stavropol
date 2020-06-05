939810a0fbac25dd7978943ec3463d340b3c6938
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-06-05 22:43:40 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var mySwiper = new Swiper ('.swiper-container', {
   // Optional parameters
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 90,
   // If we need pagination
   pagination: {
@@ -14,4 +14,21 @@ var mySwiper = new Swiper ('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      spaceBetween: 20,
+      slidesPerView: 1
+    },
+    // when window width is >= 480px
+    768: {
+      spaceBetween: 50,
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 90
+    }
+  }
 })
